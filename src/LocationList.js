946b5548df4ab838a09a0c75198dd0b3bb6ade32
Adam Razzak <a.abdel@hotmail.co.uk>
2018-08-06 19:50:53 +0100

    feat: add search function

--- src/LocationList.js
+++ src/LocationList.js
@@ -3,16 +3,45 @@ import React, { Component } from 'react';
 // import './App.css';
 
 class LocationList extends Component {
+
+  state = {
+    locations: ['york', 'london', 'londonderry', 'new york', 'yorkshire']
+
+  }
+
+  filterLocations = (event) => {
+    let searchTerm = event.target.value;
+    let trimmedSearchTerm = searchTerm.trim();
+    if (trimmedSearchTerm) {
+      console.log(trimmedSearchTerm);
+      this.filteredLocations = this.state.locations.filter((location) => location.includes(trimmedSearchTerm));
+      console.log(this.filteredLocations);
+      this.setState({filteredLocations: this.filteredLocations});
+
+    } else {
+      this.setState({filteredLocations: this.state.locations});
+    }
+  }
+
+  componentWillMount () {
+    this.setState({filteredLocations: this.state.locations});
+    this.filteredLocations = this.state.locations;
+  }
+
   render() {
+    this.locations = this.state.filteredLocations;
     return (
       <div className="location-list">
         <div className="location-list-items">
         <h2>locations</h2>
-        <input type='text' placeholder='search locations'/>
+        <input onChange={(event) => this.filterLocations(event)} type='text' placeholder='search locations'/>
         {/* made h2 as h1 taken by main app heading */}
         <div className="location-list-elements-container">
         <ul className="location-list-elements">
-          <li>location 1</li>
+          {this.locations.map((location) => (
+            <li key={location}>{location}</li>
+          ))}
+          <li></li>
           <li>location 2</li>
           <li>location 3</li>
           <li>location 4</li>
