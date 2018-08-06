cbcc080e772065c3b5d4b2d743a044a9c4954a8b
Adam Razzak <a.abdel@hotmail.co.uk>
2018-08-06 18:48:39 +0100

    feat: add location list containers, search box and list scrolling

--- src/LocationList.js
+++ src/LocationList.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
 // import { Link } from 'react-router-dom'
-import './App.css';
+// import './App.css';
 
 class LocationList extends Component {
   render() {
     return (
       <div className="location-list">
         <div className="location-list-items">
-        <p>items</p>
+        <h2>locations</h2>
+        <input type='text' placeholder='search locations'/>
+        {/* made h2 as h1 taken by main app heading */}
+        <div className="location-list-elements-container">
+        <ul className="location-list-elements">
+          <li>location 1</li>
+          <li>location 2</li>
+          <li>location 3</li>
+          <li>location 4</li>
+          <li>location 5</li>
+          <li>location 6</li>
+          <li>location 7</li>
+          <li>location 8</li>
+          <li>location 9</li>
+          <li>location 10</li>
+          <li>location 11</li>
+        </ul>
+        </div>
         </div>
         <div className="location-list-info">
-        <p>info</p>
+        <h2>location info</h2>
         </div>
       </div>
     );
